4c2e3f7ef646ed46aa72ba86252117cf794d0c5e
Ismoil <ismoil997@seznam.cz>
2024-04-28 20:18:09 +0500

    refactor: add slider n components files to gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import logo from "/logo.png";
 import "./App.css";
+import { UserCarousel } from "./Slider/UserCarousel";
 // import Modal from "./Modals/Modal";
 // import Agreement from "./Modals/Agreement";
 // import SignUp from "./Modals/SignUp";
@@ -8,35 +7,18 @@ import "./App.css";
 // import Confirmation from "./Modals/Confirmation";
 
 function App() {
-  const [count, setCount] = useState(0);
   // const [active, setActive] = useState(false);
 
   return (
-    <>
+    <div className="h-full w-full">
       {/* <Modal active={active} setActive={setActive}> */}
       {/* <LogIn active={active}  /> */}
       {/* <Confirmation active={active}  /> */}
       {/* <SignUp active={active} /> */}
       {/* <Agreement /> */}
       {/* </Modal> */}
-      <div>
-        <a href="https://meedweff.vercel.app" target="_blank">
-          <img src={logo} className="logo" alt="logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the MeedWeff logo to go to website
-      </p>
-    </>
+      <UserCarousel />
+    </div>
   );
 }
 
