77a23d1da167ec0774a82b7940b43f939d1fc268
thempthename <noname03921@gmail.com>
2024-04-28 21:29:07 +0500

    new changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,15 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 
 
@@ -16,17 +25,44 @@ const Header = () => {
     return (
         <>
             <header className='flex gap-5 w-full items-center justify-around border-1 p-10'>
-                <div className="img flex">
-                    <img src={img1} alt="" />
+                <div className='flex items-center gap-3'>
+                    <Sheet>
+                        <SheetTrigger><i className="fa-solid fa-bars"></i></SheetTrigger>
+                        <SheetContent>
+                            <SheetHeader>
+                                <SheetDescription>
+                                    <div className='soazla'>
+                                        <a className=' hvr' href="">Главная</a>
+                                        <a className=' hvr' href="">Магазин товаров</a>
+                                        <a className=' hvr' href="">Договор оферты</a>
+                                        <a className=' hvr' href="">Сертификаты</a>
+                                    </div>
+                                    <div className="tel">
+                                        <p>Телефон:</p>
+                                        <a href="tel:+998 55 519 90 90">+998 55 519 90 90</a>
+                                    </div>
+                                    <div className="fain">
+                                        <i className="fain1 fa-brands fa-facebook-f fa-2xl"></i>
+                                        <i className="fain2 fa-brands fa-instagram fa-2xl"></i>
+                                    </div>
+                                </SheetDescription>
+                            </SheetHeader>
+                        </SheetContent>
+                    </Sheet>
+                    <div className="img flex items-center gap-3">
+                        <img src={img1} alt="" />
+                    </div>
                 </div>
-                <div className='flex gap-5 items-center'>
+                <div className='flex gap-5 items-center links'>
                     <div className="links flex gap-5 items-center ">
                         <a className='link hvr' href="/">Главная</a>
                         <a className='link hvr' href="">Магазин товаров</a>
                     </div>
-                    <div className="position">
-
+                    <div className="position border flex gap-3 cursor-pointer">
+                        <i class="fa-solid fa-location-dot text-gray-600"></i>
+                        <p>улица Мукими, 166</p>
                     </div>
+
                 </div>
                 <div className='flex gap-2 items-center borderr-1'>
                     <Select>
@@ -44,7 +80,7 @@ const Header = () => {
                             <AvatarImage src="https://github.com/shadcn.png" />
                             <AvatarFallback>CN</AvatarFallback>
                         </Avatar>
-                        <p>Войти в кабинет</p>
+                        <p className='voyti'>Войти в кабинет</p>
                     </button>
                 </div>
             </header>
