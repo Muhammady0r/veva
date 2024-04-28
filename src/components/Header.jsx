764724ed780e84968aa146cf0ebf88b710d7d122
thempthename <noname03921@gmail.com>
2024-04-28 14:46:07 +0500

    some changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            something
+            anything
+            <div>things</div>
+        </div>
+    )
+}
+
+export default Header
